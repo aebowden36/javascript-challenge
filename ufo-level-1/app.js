69306ba35668d3a733cbe6ed827efd10befb2936
Alyssa <aebowden36@gmail.com>
2021-06-24 19:47:46 -0400

    filter

--- ufo-level-1/app.js
+++ ufo-level-1/app.js
@@ -29,14 +29,34 @@ data.forEach(function(tableData){
 });
 
 var button = d3.select('#filter-btn');
+var form = d3.select('#form')
 
 button.on('click', runEnter);
-FormData.on("submit", runEnter);
+form.on("submit", runEnter);
 
 function runEnter(){
+
     d3.event.preventDefault();
 
     var inputElement = d3.select ("#datetime");
     var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredInfo = tableData.filter(item => item.datetime === inputValue);
     
-}
+    console.log(filteredInfo)
+    
+    var tbody = d3.select("tbody");
+
+    filteredInfo.forEach(function(info){
+        var row = tbody.append("tr");
+        Object.entries(info).forEach(function([key,value]){
+            console.log(key,value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+runEnter(tableData);
